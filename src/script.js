7e8fb03c98e59a488af133a1f438aa04f4fa3672
Nikil <nik300601@gmail.com>
2024-01-12 17:00:49 +0000

    Added input form that creates new to-do element

--- src/script.js
+++ src/script.js
@@ -4,13 +4,23 @@ function pageLoad() {
 
 }
 
-class toDoObject {
-    constructor(title, details, checked, projectType, dueDate) {
-        this.title = title;
-        this.details = details;
-        this.projectType = projectType;
-        this.dueDate = dueDate;
-        createToDoElement()
+class Form {
+    constructor() {
+        CreateFormElement()
+    }
+}
+
+class ToDoObject {
+    constructor(formTitle, formDetails, formDueDate, formChecked, formProjectType) {
+        this.title = formTitle;
+        this.details = formDetails;
+        this.checked = formChecked;
+        this.projectType = formProjectType;
+        this.dueDate = formDueDate;
+        CreateToDoElement(this.title, this.details, this.date, this.checked, this.projectType, this.dueDate)
+
+
+
     }
     edit() {
 
@@ -22,7 +32,7 @@ class toDoObject {
 
 }
 
-function createToDoElement() {
+function CreateToDoElement(formTitle) {
     const newToDo = document.createElement('div');
     newToDo.classList.add('to-do-element');
 
@@ -33,21 +43,75 @@ function createToDoElement() {
 
     const title = document.createElement('div')
     title.classList.add('to-do-title')
+    title.textContent = formTitle
     newToDo.appendChild(title)
 
-    const detailsBtn = document.createElement('div')
+    const detailsBtn = document.createElement('button')
     detailsBtn.classList.add('detailsBtn')
     newToDo.appendChild(detailsBtn)
 
-    const date = document.createElement('button')
+    const date = document.createElement('div')
     date.classList.add('date')
     newToDo.appendChild(date)
 
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('editBtn')
+    newToDo.appendChild(editBtn)
 
+    const delBtn = document.createElement('button')
+    delBtn.classList.add('delBtn')
+    newToDo.appendChild(delBtn)
 
     const contentArea = document.querySelector('.main-area');
     contentArea.appendChild(newToDo)
 
 }
 
-const todo = new toDoObject
+function CreateFormElement() {
+    const newForm = document.createElement('div')
+    newForm.classList.add('form-element')
+
+    const title = document.createElement('input')
+    title.classList.add('form-title')
+    newForm.appendChild(title)
+
+    const details = document.createElement('input')
+    details.classList.add('form-details')
+    newForm.appendChild(details)
+
+    const date = document.createElement('input')
+    date.classList.add('form-date')   
+    date.type = 'date'
+    newForm.appendChild(date)
+
+    const submit = document.createElement('button')
+    submit.classList.add('form-submit')
+    newForm.appendChild(submit)
+    submit.addEventListener('click', Submit)
+
+
+
+    const newFormBackground = document.createElement('div')
+    newFormBackground.classList.add('form-background-overlay')
+    newFormBackground.appendChild(newForm)
+
+    const page = document.querySelector('html')
+    page.appendChild(newForm)
+    page.appendChild(newFormBackground)
+}
+
+function Submit() {
+    const formTitle = document.querySelector('.form-title')
+    const formDetails = document.querySelector('.form-details')
+    const formDate = document.querySelector('.form-date')
+
+    const newToDoObj = new ToDoObject(formTitle.value, formDetails.value, formDate.value)
+}
+
+
+
+
+
+// const todo = new ToDoObject
+
+const newform = new Form
