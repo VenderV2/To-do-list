bf93b2e40896cd948f01b4019268d38b63a19245
Nikil <nik300601@gmail.com>
2024-01-12 20:01:50 +0000

    added edit function and am now storing all entry objectsand ids into arrays

--- src/script.js
+++ src/script.js
@@ -1,10 +1,6 @@
 // import * as toDo from "to-do-functions.js"
-
-function pageLoad() {
-
-}
-
-
+const library = [];
+const IDs = [];
 
 class ToDoObject {
     constructor(formTitle, formDetails, formDueDate, formChecked, formProjectType) {
@@ -13,13 +9,12 @@ class ToDoObject {
         this.checked = formChecked;
         this.projectType = formProjectType;
         this.dueDate = formDueDate;
-        CreateToDoElement(this.title, this.details, this.dueDate, this.checked, this.projectType)
-
+        this.ID = CreateToDoElement(this.title, this.details, this.dueDate, this.checked, this.projectType)
 
 
     }
     edit() {
-
+        
     }
 
     delete() {
@@ -32,8 +27,20 @@ class Form {
     constructor() {
         CreateFormElement()
     }
-    delete() {
-        deleteFormObj()
+    static delete() {
+        const form = document.querySelector('.form-element')
+        const formBackground = document.querySelector('.form-background-overlay')
+        form.remove()
+        formBackground.remove()
+    }
+    static Submit() {
+        const formTitle = document.querySelector('.form-title')
+        const formDetails = document.querySelector('.form-details')
+        const formDate = document.querySelector('.form-date')
+    
+        const newToDoObj = new ToDoObject(formTitle.value, formDetails.value, formDate.value)
+        library.push(newToDoObj)
+        IDs.push(newToDoObj.ID)
     }
 }
 
@@ -43,6 +50,7 @@ class Form {
 function CreateToDoElement(objTitle, objDetails, objDueDate, objChecked, objProjectType) {
     const newToDo = document.createElement('div');
     newToDo.classList.add('to-do-element');
+    newToDo.id = objDetails.replace(/ /g,'')
 
     const checkbox = document.createElement('input')
     checkbox.type = 'checkbox'
@@ -52,8 +60,13 @@ function CreateToDoElement(objTitle, objDetails, objDueDate, objChecked, objProj
 
     const title = document.createElement('div')
     title.classList.add('to-do-title')
+    //title.id = newToDo.id
     title.textContent = objTitle
     newToDo.appendChild(title)
+
+    const details = document.createElement('div')
+    details.classList.add('details')
+    details.textContent = objDetails
 
     const detailsBtn = document.createElement('button')
     detailsBtn.classList.add('detailsBtn')
@@ -67,7 +80,9 @@ function CreateToDoElement(objTitle, objDetails, objDueDate, objChecked, objProj
 
     const editBtn = document.createElement('button')
     editBtn.classList.add('editBtn')
-    editBtn.textContent = 'Edit'
+    //editBtn.id = newToDo.id
+    editBtn.textContent = 'Edit'                                      //unfinished
+    editBtn.addEventListener('click', () => {editEntry(newToDo.id)})
     newToDo.appendChild(editBtn)
 
     const delBtn = document.createElement('button')
@@ -77,6 +92,8 @@ function CreateToDoElement(objTitle, objDetails, objDueDate, objChecked, objProj
 
     const contentArea = document.querySelector('.main-area');
     contentArea.appendChild(newToDo)
+
+    return  newToDo.id
 
 }
 
@@ -103,12 +120,12 @@ function CreateFormElement() {
     submit.classList.add('form-submit')
     submit.textContent = 'Submit'
     newForm.appendChild(submit)
-    submit.addEventListener('click', Submit)
+    submit.addEventListener('click', Form.Submit)
 
     const delBtn = document.createElement('button')
     delBtn.classList.add('delBtn')
-    delBtn.textContent = 'Delete'
-    delBtn.addEventListener('click', deleteFormObj)
+    delBtn.textContent = 'Cancel'
+    delBtn.addEventListener('click', Form.delete)
     newForm.appendChild(delBtn)
 
 
@@ -121,24 +138,56 @@ function CreateFormElement() {
     page.appendChild(newFormBackground)
 }
 
-//submit button on new entry form object
-function Submit() {
-    const formTitle = document.querySelector('.form-title')
-    const formDetails = document.querySelector('.form-details')
-    const formDate = document.querySelector('.form-date')
+function editEntry(entryID) {
+    const entryTitle = document.getElementById(entryID).getElementsByClassName('to-do-title')[0]
+    const entryDetails = document.getElementById(entryID).getElementsByClassName('details')[0]
 
-    const newToDoObj = new ToDoObject(formTitle.value, formDetails.value, formDate.value)
-}
-
-function deleteFormObj() {
-    const form = document.querySelector('.form-element')
-    const formBackground = document.querySelector('.form-background-overlay')
-    form.remove()
-    formBackground.remove()
+    CreateEditForm(entryTitle.textContent, entryDetails.textContent)
 }
 
 
 
-// const todo = new ToDoObject
+function CreateEditForm(objTitle, objDetails, objDueDate, objChecked, objProjectType) {
+    const newForm = document.createElement('div')
+    newForm.classList.add('form-element')
+
+    const title = document.createElement('input')
+    title.classList.add('form-title')
+    newForm.appendChild(title)
+    title.value = objTitle
+    
+
+    const details = document.createElement('input')
+    details.classList.add('form-details')
+    newForm.appendChild(details)
+    details.value = objDetails
+
+    const date = document.createElement('input')
+    date.classList.add('form-date')   
+    date.type = 'date'
+    newForm.appendChild(date)
+    date.value = objDueDate
+
+    const confirm = document.createElement('button')
+    confirm.classList.add('form-confirm')
+    confirm.textContent = 'Confirm'
+    newForm.appendChild(confirm)
+    // confirm.addEventListener('click', )                     //unfinished
+
+    const delBtn = document.createElement('button')
+    delBtn.classList.add('delBtn')
+    delBtn.textContent = 'Cancel'
+    delBtn.addEventListener('click', Form.delete)
+    newForm.appendChild(delBtn)
+
+
+    const newFormBackground = document.createElement('div')
+    newFormBackground.classList.add('form-background-overlay')
+    newFormBackground.appendChild(newForm)
+
+    const page = document.querySelector('html')
+    page.appendChild(newForm)
+    page.appendChild(newFormBackground)
+}
 
 const newform = new Form
